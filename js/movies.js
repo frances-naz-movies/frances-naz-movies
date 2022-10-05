2dc03103f43cb2e47766d55c65feef2f5f873740
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-10-05 12:26:54 -0500

    Able to add Movies without refreshing the page

--- js/movies.js
+++ js/movies.js
@@ -57,28 +57,10 @@ $("#addMovieSubmit").click(function(e){
         body: JSON.stringify(movieToPOST)
     }
     function addMovie(){
-        fetch(moviesURL, postOptions).then(response => response.json().then(data => console.log(data)))
+        fetch(moviesURL, postOptions).then(response => response.json().then(data => getMovies()))
     }
     addMovie();
-
 
 })
 
 
-
-// const exampleModal = document.getElementById('exampleModal')
-// exampleModal.addEventListener('show.bs.modal', event => {
-//     // Button that triggered the modal
-//     const button = event.relatedTarget
-//     // Extract info from data-bs-* attributes
-//     const recipient = button.getAttribute('data-bs-whatever')
-//     // If necessary, you could initiate an AJAX request here
-//     // and then do the updating in a callback.
-//     //
-//     // Update the modal's content.
-//     const modalTitle = exampleModal.querySelector('.modal-title')
-//     const modalBodyInput = exampleModal.querySelector('.modal-body input')
-//
-//     modalTitle.textContent = `New message to ${recipient}`
-//     modalBodyInput.value = recipient
-// })
