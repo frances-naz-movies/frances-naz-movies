de3f752c758ce19948baf2c2853b8c02c939888d
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-10-05 14:08:15 -0500

    Added trash button

--- js/movies.js
+++ js/movies.js
@@ -11,6 +11,10 @@ function getMovies(){
             data.forEach((movies, i) => {
                 $("#moviesOutput").append(`
             <div class="card mb-3" style="max-width: 540px;">
+            <svg id="deleteButton" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style="position:absolute; margin-left: 520px; margin-top: 5px">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg>
               <div class="row g-0">
                 <div class="col-md-4">
                   <img src="${data[i].poster}" class="img-fluid rounded-start" alt="Movie Poster" style="width:200px; height:300px">
@@ -63,4 +67,6 @@ $("#addMovieSubmit").click(function(e){
 
 })
 
-
+    $("#deleteButton").on('click', function () {
+        alert("It worked!")
+    })
